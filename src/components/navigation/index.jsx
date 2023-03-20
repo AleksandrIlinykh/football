b31a1db008192a5c878584b7884ea72f7908ed80
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2023-03-20 23:12:01 +0100

    init

---
+++ src/components/navigation/index.jsx
@@ -0,0 +1,33 @@
+import cn from 'classnames';
+import Link from 'next/link';
+import { useState } from 'react';
+import styles from './navigation.module.css';
+import { usePathname } from 'next/navigation';
+
+export default function Navigation() {
+  const [currenaPageName, setCurrentPageName] = useState('');
+  const pathname = usePathname();
+
+  const pagesNames = [
+    { name: 'Главная', route: '/' },
+    { name: 'Записаться', route: '/enroll' },
+    { name: 'О тренере', route: '/about' },
+    { name: 'Первая тренировка', route: '/first-lesson' },
+  ];
+
+  return (
+    <section className={styles.container}>
+      {pagesNames.map(({ name, route }, index) => (
+        <Link
+          href={`${route}`}
+          key={index}
+          className={cn(styles.link, {
+            [styles.active]: pathname === route,
+          })}
+        >
+          {name}
+        </Link>
+      ))}
+    </section>
+  );
+}

c80a52f20ace6ef28371a9cddf1752adf93296d9
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2023-03-27 23:27:36 +0200

    1st page layout

--- src/pages/first-lesson/index.js
+++ src/pages/first-lesson/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import styles from './index.module.css';
 import Navigation from '@/components/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <>
+    <section className={styles.main}>
       <Navigation />
-    </>
+
+      <main className={styles.main}></main>
+    </section>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,40 @@
 import styles from './index.module.css';
 import { Inter } from 'next/font/google';
 import Navigation from '@/components/navigation';
-
+import classNames from 'classnames';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <section className={styles.main}>
+    <section className={styles.page}>
       <Navigation />
 
-      <main className={styles.main}></main>
+      <main className={styles.content}>
+        <section className={styles.mainImgContainer}>
+          <div className={styles.mainImgTexts}>
+            <h1 className={styles.h1}>Hightrick</h1>
+            <p className={styles.p}>Школа фристайла и футбольной техники</p>
+          </div>
+
+          <div className={styles.mainImg}>
+            <img className={styles.img} src="/main.png" alt="чел с мячём" />
+          </div>
+        </section>
+
+        <h2 className={classNames(styles.h2, styles.subHeader)}>
+          Что такое футбольный <br /> фристайл
+        </h2>
+        <p className={classNames(styles.p, styles.bottomParagraph)}>
+          Футбольный фристайл - красивый и зрелищный вид спорта, суть которого
+          заключается в выполнении различных трюков с футбольным мячом. Для
+          этого не нужно иметь талант.
+        </p>
+        <div className={styles.balls}>
+          <img className={styles.img} src="/balls.png" alt="чисто мячи" />
+        </div>
+
+        <button>Записаться</button>
+      </main>
     </section>
   );
 }

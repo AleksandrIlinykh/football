3539a4f8317c63c98b901a618d084b56cd3c01b7
ailinykh <aleksandr.ilinykh@isax.com>
2023-04-16 21:06:31 +0200

    all pages init mockup

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,18 +1,18 @@
-import cn from 'classnames';
-import Link from 'next/link';
-import { useState } from 'react';
-import styles from './navigation.module.css';
-import { usePathname } from 'next/navigation';
+import cn from "classnames";
+import Link from "next/link";
+import { useState } from "react";
+import styles from "./navigation.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
-  const [currenaPageName, setCurrentPageName] = useState('');
+  const [currenaPageName, setCurrentPageName] = useState("");
   const pathname = usePathname();
 
   const pagesNames = [
-    { name: 'Главная', route: '/' },
-    { name: 'О школе', route: '/about-school' },
-    { name: 'О тренере', route: '/about' },
-    { name: 'Первая тренировка', route: '/first-lesson' },
+    { name: "Главная", route: "/" },
+    { name: "О школе", route: "/about-school" },
+    { name: "О тренере", route: "/about-trainer" },
+    { name: "Первая тренировка", route: "/first-lesson" },
   ];
 
   return (
